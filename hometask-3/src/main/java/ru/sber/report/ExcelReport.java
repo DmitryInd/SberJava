3912aa8ff7c049b53eea9afcfec612074b72c42c
Дмитрий Евгеньевич Инденбом <indenbom.de@phystech.edu>
2021-05-12 19:51:40 +0300

    Внесена часть требуемых изменений

--- hometask-3/src/main/java/ru/sber/report/ExcelReport.java
+++ hometask-3/src/main/java/ru/sber/report/ExcelReport.java
@@ -7,27 +7,32 @@ import java.io.IOException;
 import java.io.OutputStream;
 
 public class ExcelReport implements Report {
-    @Override
-    public byte[] asBytes() {
-        ByteArrayOutputStream output = new ByteArrayOutputStream();
-        writeTo(output);
-
-        return output.toByteArray();
-    }
-
-    @Override
-    public void writeTo(OutputStream os) {
-        try {
-            book.write(os);
-            book.close();
-        } catch (IOException e) {
-            throw new RuntimeException("Couldn't write", e);
-        }
-    }
+    private final Workbook book;
 
     public ExcelReport(Workbook book) {
         this.book = book;
     }
 
-    private final Workbook book;
+    @Override
+    public byte[] asBytes() {
+        byte[] reportAsBytes;
+        try(ByteArrayOutputStream output = new ByteArrayOutputStream()) {
+            writeTo(output);
+            reportAsBytes = output.toByteArray();
+            output.close();
+        } catch (IOException e) {
+            throw new RuntimeException("Couldn't convert to bytes", e);
+        }
+
+        return reportAsBytes;
+    }
+
+    @Override
+    public void writeTo(OutputStream os) {
+        try(book){
+            book.write(os);
+        } catch (IOException e) {
+            throw new RuntimeException("Couldn't write", e);
+        }
+    }
 }
