5ab7aea2ab141ae3daeb6a3816dcd6a5d0d645dc
Дмитрий Евгеньевич Инденбом <indenbom.de@phystech.edu>
2021-11-17 20:11:00 +0300

    Исправление ошибок

--- src/main/java/ru/sber/Application.java
+++ src/main/java/ru/sber/Application.java
@@ -1,11 +1,16 @@
 package ru.sber;
 
 import java.io.IOException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Application {
+    private static final Logger logger = LogManager.getRootLogger();
+
     public static void main(String... args) throws IOException {
         IndividualClient myClient = (IndividualClient) ClientReader.optimalCreateClient(
                 "src/test/resources/IndividualObject.json");
-        System.out.println(myClient.toString());
+
+        logger.info("Object with card data: " + myClient.toString());
     }
 }
